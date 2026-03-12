999e8db127d02b7e325ce08dc418367dd0ae6285
seetharamanrs <seetharaman2632001@gmail.com>
2026-03-12 20:21:09 +0530

    Load_expenses.py

---
+++ src/load_expenses.py
@@ -0,0 +1,12 @@
+import pandas as pd
+
+df=pd.read_csv(r"D:\my_git\ai_expense_analyzer\data\expenses.csv")
+
+print("Dataset Preview:")
+print(df.head())
+
+print("\nColumns:")
+print(df.columns)
+
+print("\nTotal Spending:")
+print(df["amount"].sum())
