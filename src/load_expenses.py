4fa8f8bdff5ace8034521116de08d16fa309be91
seetharamanrs <seetharaman2632001@gmail.com>
2026-03-15 21:10:41 +0530

    Load_expenses.py

--- src/load_expenses.py
+++ src/load_expenses.py
@@ -3,7 +3,7 @@ import pandas as pd
 df=pd.read_csv(r"D:\my_git\ai_expense_analyzer\data\expenses.csv")
 
 print("Dataset Preview:")
-print(df.head())
+print(df.head(5))
 
 print("\nColumns:")
 print(df.columns)
