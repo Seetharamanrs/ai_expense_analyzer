533e518fb5013394dc39c916ab480325be39efa9
seetharamanrs <seetharaman2632001@gmail.com>
2026-04-02 15:03:14 +0530

    Feat: Budget suggesstion
    Overspending Advice are added

--- src/ai_advisor.py
+++ src/ai_advisor.py
@@ -7,8 +7,21 @@ from dotenv import load_dotenv
 load_dotenv()
 
 client = OpenAI(api_key=os.getenv("OPENAI_API_KEY"))
+
+def get_response(data_summary,instruction):
+    
+    response=client.chat.completions.create(
+        model="gpt-4o-mini",
+        messages=[
+            {"role":"system","content":"You are a financial advisor. Give very short, direct answer (max 2-3 bullet points), Every thing in Rupees "},
+            {"role":"user","content":f"Here is my spending:\n {data_summary}\n{instruction}"}
+        ]
+    )
+
+    return response.choices[0].message.content
+
 def get_financial_advice(summary):
-    return get_response(summary, "Give 3 short actionable financial tips.")
+    return get_response(summary, "Give 3 very short actionable financial tips.")
 
 
 def get_overspending_advice(summary):
@@ -16,18 +29,7 @@ def get_overspending_advice(summary):
 
 
 def get_budget_suggestion(summary):
-    return get_response(summary, "Suggest a simple monthly budget based on this spending.")
+    return get_response(summary, "Suggest a simple monthly budget in 3 short points.")
 
-def get_response(data_summary,instruction):
-    
-    response=client.chat.completions.create(
-        model="gpt-4o-mini",
-        messages=[
-            {"role":"system","content":"You are a financial advisor. "},
-            {"role":"user","content":f"Here is my spending:\n {data_summary}\n{instruction}"}
-        ]
-    )
-
-    return response.choices[0].message.content
 
 
